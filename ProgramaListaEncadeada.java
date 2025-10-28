a16354243efc906643e292bedad2720d9d79b7b9
Mel Shine <144854854+mel504@users.noreply.github.com>
2025-10-28 10:48:52 -0300

    Fix RA number for Melissa Namie Shine
    Updated the RA number for Melissa Namie Shine.

--- ProgramaListaEncadeada.java
+++ ProgramaListaEncadeada.java
@@ -1,7 +1,7 @@
 /*  
     GRUPO
     Giovanna Borges Coelho - RA 10756784
-    Melissa Namie Shine - RA
+    Melissa Namie Shine - RA 10401096
 
     Descrição: especializacao de ListaEncadeada<Linha> para armazenar o programa
     ordenado por numero da linha
